de9a33e41c488b9181eced6e59a0b54762ae4357
demosthenesd <demosthenesd@icloud.com>
2023-08-20 02:56:28 +1000

    might do News API to mashup with the balldontlie api | it works

---
+++ routes/news.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+const axios = require("axios");
+
+const news_api_key = "01b547fcc87a451cabb12e89063972f8";
+const query = "Mikal Bridges";
+
+const URL = `https://newsapi.org/v2/everything?q=${query}&from=2023-07-19&sortBy=relevancy&apiKey=${news_api_key}`;
+
+async function fetchData() {
+  await axios.get(URL).then((res) => console.log(res.data.articles));
+}
+fetchData();
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
