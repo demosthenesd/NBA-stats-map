5f8680b3ae4f54e2377a27c0d70ac77e81017979
demosthenesd <demosthenesd@icloud.com>
2023-08-22 11:01:52 +1000

    API mashup now displays 3 | Stats, News, Highlights

--- routes/news.js
+++ routes/news.js
@@ -3,16 +3,33 @@ var router = express.Router();
 const axios = require("axios");
 
 const news_api_key = "01b547fcc87a451cabb12e89063972f8";
-const query = "Mikal Bridges";
 
-const URL = `https://newsapi.org/v2/everything?q=${query}&from=2023-07-19&sortBy=relevancy&apiKey=${news_api_key}`;
+async function fetchData(URL) {
+  try {
+    const response = await axios.get(URL);
+    return response.data.articles;
+  } catch (err) {
+    console.log(err);
 
-async function fetchData() {
-  await axios.get(URL).then((res) => console.log(res.data.articles));
+    return "Error occured";
+  }
 }
 
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+router.post("/", async function (req, res, next) {
+  try {
+    const { player, season } = req.body;
+
+    const URL = `https://newsapi.org/v2/everything?q=${player} Basketball&pageSize=4&language=en&sortBy=relevancy&apiKey=${news_api_key}`;
+
+    const result = await fetchData(URL);
+    res.json(result);
+  } catch (error) {
+    res.status(500).send("An error occurred.");
+  }
 });
 
 module.exports = router;
