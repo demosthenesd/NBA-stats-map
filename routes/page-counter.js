86cbeeecea09731d34a3f510cae5366d5dc589ea
demosthenesd <demosthenesd@icloud.com>
2023-09-14 23:23:43 +1000

    Update page-counter.js

--- routes/page-counter.js
+++ routes/page-counter.js
@@ -9,6 +9,8 @@ const bucketName = process.env.AWS_BUCKET_NAME;
 const key = process.env.AWS_JSON_FILE_NAME;
 
 router.get("/", async (req, res) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second
+
   const results = await readJsonFile(bucketName, key); //read page counter JSON file from s3
   res.json({ results });
 });
